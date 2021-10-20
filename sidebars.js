dd6d863a39bedff006b94ef68947b597e999d481
Kim Pohas <kim.pohas@saucelabs.com>
2021-10-20 02:41:18 -0700

    WIP: Visual FAQ

--- sidebars.js
+++ sidebars.js
@@ -811,6 +811,7 @@ module.exports = {
         'visual/css-animations',
         'visual/acct-team-mgmt',
         'visual/notifications',
+        'visual/faq',
         ],
         "Reference": [
             'dev/glossary',
